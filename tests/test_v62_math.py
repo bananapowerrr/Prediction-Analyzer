3937497239dae807ba951c3bf585d52c43271ad3
Your Name <you@example.com>
2026-08-28 07:12:52 +0300

    auto: Создай tests/test_v62_math.py с функцией test_add(), которая

---
+++ tests/test_v62_math.py
@@ -0,0 +1,9 @@
+import unittest
+from utils.v62_math import add
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+
+if __name__ == '__main__':
+    unittest.main()
