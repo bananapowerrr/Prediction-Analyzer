b9970f4ff1da383e99a25aec2456c01d91d2f2cd
Your Name <you@example.com>
2026-08-28 15:14:09 +0300

    auto: По файлу tests/test_smoke_imports.py сделай: - Убедись что к

--- tests/test_smoke_imports.py
+++ tests/test_smoke_imports.py
@@ -9,28 +9,30 @@ import core.models
 import execution.paper
 
 class TestSmokeImports(unittest.TestCase):
-    def test_import_config(self):
+    """Тесты на импорт модулей"""
+
+    def test_import_config(self) -> None:
         self.assertIsNotNone(config)
 
-    def test_import_data_filters(self):
+    def test_import_data_filters(self) -> None:
         self.assertIsNotNone(data.filters)
 
-    def test_import_risk_engine(self):
+    def test_import_risk_engine(self) -> None:
         self.assertIsNotNone(risk_engine)
 
-    def test_import_main(self):
+    def test_import_main(self) -> None:
         self.assertIsNotNone(main)
 
-    def test_import_data_scanner(self):
+    def test_import_data_scanner(self) -> None:
         self.assertIsNotNone(data.scanner)
 
-    def test_import_data_polymarket_client(self):
+    def test_import_data_polymarket_client(self) -> None:
         self.assertIsNotNone(data.polymarket_client)
 
-    def test_import_core_models(self):
+    def test_import_core_models(self) -> None:
         self.assertIsNotNone(core.models)
 
-    def test_import_execution_paper(self):
+    def test_import_execution_paper(self) -> None:
         self.assertIsNotNone(execution.paper)
 
 if __name__ == '__main__':
