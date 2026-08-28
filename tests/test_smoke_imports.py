ab5ddf8c96ca428a9fa5caeae8248fc91658aa43
Your Name <you@example.com>
2026-08-28 12:41:58 +0300

    auto: Тест: import main, data.scanner, data.polymarket_client, cor

--- tests/test_smoke_imports.py
+++ tests/test_smoke_imports.py
@@ -1,3 +1,4 @@
+import unittest
 import config
 import data.filters
 import risk_engine
@@ -6,3 +7,31 @@ import data.scanner
 import data.polymarket_client
 import core.models
 import execution.paper
+
+class TestSmokeImports(unittest.TestCase):
+    def test_import_config(self):
+        self.assertIsNotNone(config)
+
+    def test_import_data_filters(self):
+        self.assertIsNotNone(data.filters)
+
+    def test_import_risk_engine(self):
+        self.assertIsNotNone(risk_engine)
+
+    def test_import_main(self):
+        self.assertIsNotNone(main)
+
+    def test_import_data_scanner(self):
+        self.assertIsNotNone(data.scanner)
+
+    def test_import_data_polymarket_client(self):
+        self.assertIsNotNone(data.polymarket_client)
+
+    def test_import_core_models(self):
+        self.assertIsNotNone(core.models)
+
+    def test_import_execution_paper(self):
+        self.assertIsNotNone(execution.paper)
+
+if __name__ == '__main__':
+    unittest.main()
